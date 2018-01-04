d07337f82009d39cf8ca73778f18dc7b7e9d442a
Jonathan Tung <jtung23@gmail.com>
2018-01-03 19:53:06 -0800

    transferred all to daily_data.py

--- bin/daily_data.py
+++ bin/daily_data.py
@@ -21,7 +21,10 @@ graph = facebook.GraphAPI(access_token=access_token,
 
 
 all_restaurants = db.all_restaurants
+all_ids = db.all_ids
 
+# gets data from all_ids. adds initial data
+allids = list(all_ids.find())
 restaurants = list(all_restaurants.find())
 
 yelp_ids = []
@@ -56,7 +59,7 @@ for each in yelp_ids:
 		# '$set': {'rating': value['rating']}
 	)
 
-print('yelp done')
+print('yelp update done')
 
 # Facebook update
 for each in fb_ids:
@@ -80,3 +83,79 @@ for each in fb_ids:
 	})
 
 print('fb done')
+
+# 
+# GETS NEW FIRMS AND ADDS TO RESTAURANT COLLECTION
+# 
+
+# finds missing Id that needs to be added to all_restaurant
+yelp_ids = []
+test_ids = []
+for datum in allids:
+	yelp_ids.append({'yelpId': datum['yelpId'], 'fbId': datum['fbId']})
+for val in test:
+	test_ids.append({'yelpId': val['yelpId'], 'fbId': val['fbId']})
+
+# gets array of ids that need to be added to collection
+missing_id =list(filter(lambda x: x not in test_ids, yelp_ids))
+
+headers = []
+
+for value in missing_id:
+	place_id = value['fbId']
+	search_link= place_id + '?fields=name,rating_count,checkins,overall_star_rating'
+	firms = graph.request(search_link)
+
+	r = requests.get('https://api.yelp.com/v3/businesses/' + value['yelpId'], 
+		headers={"Authorization": 'Bearer Dt0X2kf0ef_hQ5Jc_5FNnxheSlXdFX1-svTZE6AJP0J4lBoVuMFRl66QgPFblxpMN-_AHN9OL3mek81qVap7DEtTMK2MrXxXpTxV31SVTbe-qajxmCEGj_nHwuEuWnYx'}).json()
+	dat={}
+	dat['name']= r['name']
+	dat['yelpId']= r['id']
+	dat['fbId']=value['fbId']
+	dat['price'] = r.get('price')
+	dat['rating']= [{
+		'rating': r['rating'],
+		'query_date': str(now)
+	}]
+	dat['reviews']= [{
+		'review_count': r['review_count'],
+		'query_date': str(now)
+		}
+	]
+	dat['checkins']= [
+		{
+			'checkins': firms['checkins'],
+			'query_date': str(now)
+		}
+	]
+	dat['star_rating']=[
+		{
+			'overall_star_rating': firms.get('overall_star_rating'),
+			'query_date': str(now)
+		}
+	],
+	dat['rating_count']=[
+		{
+		'rating_count': firms['rating_count'],
+		'query_date': str(now)
+		}
+	],
+	dat['categories']= r['categories']
+	dat['phone']= r['display_phone']
+	dat['yelpURL']= r['url']
+	dat['yelpImg']= r['image_url']
+	dat['location']= {
+		'address': r['location']['address1'],
+		'city': r['location']['city'],
+		'state': r['location']['state'],
+		'country': r['location']['country']
+	}
+	headers.append(dat)
+
+pp.pprint(headers)
+
+for data in headers:
+	all_restaurants.update_one({'yelpId': data['yelpId']},
+		{"$set":data}, upsert=True)
+print(all_restaurants.count())
+# from IPython import embed; embed()
